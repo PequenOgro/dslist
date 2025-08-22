392d1627ac9e9c45f15d0d051de35bff56492f7e
Marcos <marcosvcmnascimento.mv@gmail.com>
2025-08-21 22:16:12 -0300

    Consulta SQL,projection,busca de games por lista

--- src/main/java/com/devsuperior/dslist/controllers/GameListController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -21,9 +21,17 @@ public class GameListController {
     @Autowired
     private GameListService gameListService;
 
+    private GameService gameService;
+
     @GetMapping
     public List<GameListDTO> findAll(){
         List<GameListDTO> result = gameListService.findAll();
+        return result;
+    }
+
+    @GetMapping(value = "/{listId}/games")
+    public List<GameMinDTO> findByList(@PathVariable Long listId){
+        List<GameMinDTO> result = gameService.findByList(listId);
         return result;
     }
 
